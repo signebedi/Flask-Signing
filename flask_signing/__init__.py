4fcb303097c1d9dfe9a757e9bbcc4220d3f3cfdc
signebedi <signe@siftingwinnowing.com>
2023-06-12 10:59:52 -0500

    Removed: old comments

--- flask_signing/__init__.py
+++ flask_signing/__init__.py
@@ -179,31 +179,3 @@ class Signatures:
             self._model = Signing
 
         return self._model
-
-
-    # def get_model(self):
-    #     class Signing(self.db.Model):
-
-    #         """
-    #         The Signing class represents the Signing table in the database.
-
-    #         Each instance of this class represents a row of data in the database table.
-
-    #         Attributes:
-    #             signature (str): The primary key of the Signing table. This field is unique for each entry.
-    #             email (str): The email associated with a specific signing key.
-    #             scope (str): The scope within which the key is valid.
-    #             active (bool): The status of the signing key. If True, the key is active.
-    #             timestamp (datetime): The date and time when the signing key was created.
-    #             expiration (datetime): The date and time when the signing key is set to expire.
-    #         """
-    #         __tablename__ = 'signing'
-    #         signature = self.db.Column(self.db.String, primary_key=True) 
-    #         email = self.db.Column(self.db.String(100))
-    #         scope = self.db.Column(self.db.String(100))
-    #         active = self.db.Column(self.db.Boolean)
-    #         timestamp = self.db.Column(self.db.DateTime, nullable=False, default=datetime.datetime.utcnow)
-    #         expiration = self.db.Column(self.db.DateTime, nullable=False, default=datetime.datetime.utcnow)
-
-    #     return Signing
-
