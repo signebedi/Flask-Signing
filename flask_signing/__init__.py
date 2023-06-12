6708b4280263a52b79d95eed5f2741bdd4defb53
signebedi <signe@siftingwinnowing.com>
2023-06-12 15:07:05 -0500

    Modified: version > 0.2.0 (#6)

--- flask_signing/__init__.py
+++ flask_signing/__init__.py
@@ -1,7 +1,7 @@
 __name__ = "flask_signing"
 __author__ = "Sig Janoska-Bedi"
 __credits__ = ["Sig Janoska-Bedi",]
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __license__ = "BSD-3-Clause"
 __maintainer__ = "Sig Janoska-Bedi"
 __email__ = "signe@atreeus.com"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='flask_signing',
-    version='0.1.0',
+    version='0.2.0',
     url='https://github.com/signebedi/Flask-Signing',
     author='Sig Janoska-Bedi',
     author_email='signe@atreeus.com',
