ed2fb5cc3fb36811397c82864b187dfb0f544163
signebedi <signe@siftingwinnowing.com>
2023-06-16 17:31:00 -0500

    Added: basic performance test suite (#34)

---
+++ tests/performance_tests.py
@@ -0,0 +1,136 @@
+# tests/performance_tests.py
+
+import logging
+import timeit
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import Session
+from flask_signing import Signatures
+
+
+# setup logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# create a file handler
+handler = logging.FileHandler('performance.log')
+handler.setLevel(logging.INFO)
+
+# create a logging format
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+# add the handlers to the logger
+logger.addHandler(handler)
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'  # Use your actual database URI
+app.secret_key = "Your_Key_Here"
+
+with app.app_context():
+    signatures = Signatures(app, byte_len=24, rate_limiting=True)
+
+session = Session()
+
+def test_write_key_to_database_performance():
+    with app.app_context():
+
+        start_time = timeit.default_timer()
+        signatures.write_key_to_database(scope='test', expiration=1, active=True, email='test@example.com', previous_key=None)
+        end_time = timeit.default_timer()
+        elapsed_time = end_time - start_time
+        logger.info(f'Performance of write_key_to_database: {elapsed_time} seconds')
+
+def test_check_key_performance():
+    with app.app_context():
+
+        key = signatures.write_key_to_database(scope='test', expiration=1, active=True, email='test@example.com', previous_key=None)
+        start_time = timeit.default_timer()
+        signatures.check_key(key, 'test')
+        end_time = timeit.default_timer()
+        elapsed_time = end_time - start_time
+        logger.info(f'Performance of check_key: {elapsed_time} seconds')
+
+def test_verify_key_performance():
+    with app.app_context():
+        signatures.rate_limiting = False  # Disable rate limiting for performance test
+
+        key = signatures.write_key_to_database(scope='test', expiration=1, active=True, email='test@example.com', previous_key=None)
+        start_time = timeit.default_timer()
+        signatures.verify_key(key, 'test')
+        end_time = timeit.default_timer()
+        elapsed_time = end_time - start_time
+        logger.info(f'Performance of verify_key: {elapsed_time} seconds')
+
+        signatures.rate_limiting = True  # Re-enable rate limiting after test
+
+
+def test_expire_key_performance():
+    with app.app_context():
+        key = signatures.write_key_to_database(scope='test', expiration=1, active=True, email='test@example.com', previous_key=None)
+        start_time = timeit.default_timer()
+        signatures.expire_key(key)
+        end_time = timeit.default_timer()
+        elapsed_time = end_time - start_time
+        logger.info(f'Performance of expire_key: {elapsed_time} seconds')
+
+def test_query_keys_performance():
+    with app.app_context():
+        # Prepare the data
+        for i in range(10):
+            signatures.write_key_to_database(scope='test', expiration=1, active=True, email='test'+str(i)+'@example.com', previous_key=None)
+
+        # Run the test
+        start_time = timeit.default_timer()
+        signatures.query_keys(active=True, scope='test', email='test5@example.com')
+        end_time = timeit.default_timer()
+        elapsed_time = end_time - start_time
+        logger.info(f'Performance of query_keys: {elapsed_time} seconds')
+
+def test_query_all_performance():
+    with app.app_context():
+        start_time = timeit.default_timer()
+        signatures.query_all()
+        end_time = timeit.default_timer()
+        elapsed_time = end_time - start_time
+        logger.info(f'Performance of query_all: {elapsed_time} seconds')
+
+
+def test_rotate_key_performance():
+    with app.app_context():
+        # Prepare the data
+        key = signatures.write_key_to_database(scope='test', expiration=1, active=True, email='test@example.com', previous_key=None)
+
+        # Run the test
+        start_time = timeit.default_timer()
+        signatures.rotate_key(key)
+        end_time = timeit.default_timer()
+        elapsed_time = end_time - start_time
+        logger.info(f'Performance of rotate_key: {elapsed_time} seconds')
+
+
+def test_rotate_keys_performance():
+    with app.app_context():
+        # Prepare the data
+        for i in range(10):
+            signatures.write_key_to_database(scope='test', expiration=1, active=True, email='test'+str(i)+'@example.com', previous_key=None)
+
+        # Run the test
+        start_time = timeit.default_timer()
+        signatures.rotate_keys(time_until=1, scope='test')
+        end_time = timeit.default_timer()
+        elapsed_time = end_time - start_time
+        logger.info(f'Performance of rotate_keys: {elapsed_time} seconds')
+
+
+if __name__ == '__main__':
+    test_write_key_to_database_performance()
+    test_check_key_performance()
+    test_verify_key_performance()
+    test_expire_key_performance()
+    test_query_keys_performance()
+    test_query_all_performance()
+    test_rotate_key_performance()
+    test_rotate_keys_performance()
+
+
