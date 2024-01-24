681b053a5c741795f95f846e4897cf6aca705f19
signebedi <signe@siftingwinnowing.com>
2024-01-24 01:27:17 -0600

    Modified: version > 0.8.0 (#50)

--- flask_signing/__metadata__.py
+++ flask_signing/__metadata__.py
@@ -1,7 +1,7 @@
 __name__ = "flask_signing"
 __author__ = "Sig Janoska-Bedi"
 __credits__ = ["Sig Janoska-Bedi",]
-__version__ = "0.7.1"
+__version__ = "0.8.0"
 __license__ = "BSD-3-Clause"
 __maintainer__ = "Sig Janoska-Bedi"
 __email__ = "signe@atreeus.com"
